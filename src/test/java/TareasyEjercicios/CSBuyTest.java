483342e35fe98883e1d91999f71e1a2cd3e3ef88
Miguel Torres <miguelanpetor93@gmail.com>
2021-03-18 18:13:45 -0600

    Actualizacion al flujo principal

--- src/test/java/TareasyEjercicios/CSBuyTest.java
+++ src/test/java/TareasyEjercicios/CSBuyTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Random;
+
 public class CSBuyTest {
     static WebDriver driver;
 
@@ -15,18 +17,41 @@ public class CSBuyTest {
 
         //Declarar elementos a usar
         WebElement fieldUsername=null, fieldPassword=null, buttonLoginBuy=null,
-                   registerBuy=null, fieldSymbol=null, fieldQuantity=null, dataTable=null;
+                   registerBuy=null, fieldSymbol=null, fieldQuantity=null, dataTableShares=null;
 
-        String username = "Juanito.Banana", password = "Testing1234";
+        String username = "Juanito.Banana", password = "Testing1234", symbolName = "MX",quantityValue = "2",shareValue,originalQuantityValue=null,originalQV=null;
 
         //Inicicar sesion
         iniciarLogin(username,password, fieldUsername,fieldPassword,buttonLoginBuy);
-        realizarCompra(registerBuy,fieldSymbol,fieldQuantity,buttonLoginBuy);
+        originalQV = realizarNuevaCompra(symbolName, quantityValue,originalQuantityValue , registerBuy,fieldSymbol,fieldQuantity,buttonLoginBuy,dataTableShares);
+        shareValue = validarNuevaCompra(dataTableShares);
+        System.out.println(originalQV + " " + shareValue);
 
     }
 
-    private static void realizarCompra(WebElement ... screenElements) {
+    private static String validarNuevaCompra(WebElement dataTableShares) {
+        dataTableShares = driver.findElement(By.xpath("//tr[contains(. , 'MX')]//td[3]"));
+        return dataTableShares.getText();
+    }
 
+    private static String realizarNuevaCompra(String symbolName, String quantityName, String originalQuantityValue, WebElement ... screenElements) {
+        //int randomNumber = new Random().nextInt(100000);
+        screenElements[4] = driver.findElement(By.xpath("//tr[contains(. , 'MX')]//td[3]"));
+        if (screenElements[4].isDisplayed()){
+            originalQuantityValue = screenElements[4].getText();
+        }
+
+        screenElements[0] = driver.findElement(By.cssSelector("[href='/buy']"));
+        screenElements[0].click();
+
+        screenElements[1] = driver.findElement(By.cssSelector("[name='symbol']"));
+        screenElements[2] = driver.findElement(By.cssSelector("[name='qty']"));
+        screenElements[3] = driver.findElement(By.cssSelector(".btn"));
+        screenElements[1].sendKeys(symbolName);
+        screenElements[2].sendKeys(quantityName);
+        screenElements[3].click();
+
+        return originalQuantityValue;
     }
 
     private static void iniciarLogin(String username, String password, WebElement ... screenelements) {
